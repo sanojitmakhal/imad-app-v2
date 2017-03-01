8a784a7dfb9b6340c57767fe00b8e3c7ea26e8de
sanojitmakhal <sanojit.kr.makhal@gmail.com>
2017-03-01 16:15:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var element=document.getElementById('madi');
+function moveRight(){
+    marginLeft-marginleft+1;
+    img.style.marginLeft=marginLeft + 'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
